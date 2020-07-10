7de1eb9a1df53a940de297f690fd50a725fb0aa9
RamyaKadali <ramya.kadali@digitalbarriers.com>
2020-07-10 15:45:58 +0100

    commit

--- VodafonePOC/AddScheduleSave.UserCode.cs
+++ VodafonePOC/AddScheduleSave.UserCode.cs
@@ -22,7 +22,7 @@ using Ranorex.Core.Testing;
 
 namespace VodafonePOC
 {
-    public partial class AddSchedule
+    public partial class AddScheduleSave
     {
         /// <summary>
         /// This method gets called right after the recording has been started.
--- VodafonePOC/AddScheduleSave.cs
+++ VodafonePOC/AddScheduleSave.cs
@@ -24,29 +24,29 @@ namespace VodafonePOC
 {
 #pragma warning disable 0436 //(CS0436) The type 'type' in 'assembly' conflicts with the imported type 'type2' in 'assembly'. Using the type defined in 'assembly'.
     /// <summary>
-    ///The AddSchedule recording.
+    ///The AddScheduleSave recording.
     /// </summary>
     [TestModule("429990a3-1458-4dfd-990c-16900f0aa5c1", ModuleType.Recording, 1)]
-    public partial class AddSchedule : ITestModule
+    public partial class AddScheduleSave : ITestModule
     {
         /// <summary>
         /// Holds an instance of the VodafonePOCRepository repository.
         /// </summary>
         public static VodafonePOCRepository repo = VodafonePOCRepository.Instance;
 
-        static AddSchedule instance = new AddSchedule();
+        static AddScheduleSave instance = new AddScheduleSave();
 
         /// <summary>
         /// Constructs a new instance.
         /// </summary>
-        public AddSchedule()
+        public AddScheduleSave()
         {
         }
 
         /// <summary>
         /// Gets a static instance of this recording.
         /// </summary>
-        public static AddSchedule Instance
+        public static AddScheduleSave Instance
         {
             get { return instance; }
         }
@@ -79,36 +79,32 @@ namespace VodafonePOC
 
             Init();
 
-            Report.Log(ReportLevel.Info, "Mouse", "Mouse Left Click item 'ApplicationUnderTest.AddDay' at 172;14.", repo.ApplicationUnderTest.AddDayInfo, new RecordItemIndex(0));
-            repo.ApplicationUnderTest.AddDay.Click("172;14");
+            Report.Log(ReportLevel.Info, "Mouse", "Mouse Left Click item 'ApplicationUnderTest.AddDay' at 211;15.", repo.ApplicationUnderTest.AddDayInfo, new RecordItemIndex(0));
+            repo.ApplicationUnderTest.AddDay.Click("211;15");
             Delay.Milliseconds(0);
             
-            Report.Log(ReportLevel.Info, "Mouse", "Mouse Left Click item 'ApplicationUnderTest.NgScope' at 60;291.", repo.ApplicationUnderTest.NgScopeInfo, new RecordItemIndex(1));
-            repo.ApplicationUnderTest.NgScope.Click("60;291");
+            Report.Log(ReportLevel.Info, "Mouse", "Mouse Left Click item 'ApplicationUnderTest.AddDay' at 35;46.", repo.ApplicationUnderTest.AddDayInfo, new RecordItemIndex(1));
+            repo.ApplicationUnderTest.AddDay.Click("35;46");
             Delay.Milliseconds(0);
             
-            Report.Log(ReportLevel.Info, "Mouse", "Mouse Left Click item 'ApplicationUnderTest.NgScope' at 340;256.", repo.ApplicationUnderTest.NgScopeInfo, new RecordItemIndex(2));
-            repo.ApplicationUnderTest.NgScope.Click("340;256");
+            Report.Log(ReportLevel.Info, "Mouse", "Mouse Left Click item 'ApplicationUnderTest.NgScope' at 346;229.", repo.ApplicationUnderTest.NgScopeInfo, new RecordItemIndex(2));
+            repo.ApplicationUnderTest.NgScope.Click("346;229");
             Delay.Milliseconds(0);
             
-            Report.Log(ReportLevel.Info, "Set value", "Setting attribute TagValue to 'number:0' on item 'ApplicationUnderTest.AddTimeHours'.", repo.ApplicationUnderTest.AddTimeHoursInfo, new RecordItemIndex(3));
-            repo.ApplicationUnderTest.AddTimeHours.Element.SetAttributeValue("TagValue", "number:0");
-            Delay.Milliseconds(20);
-            
-            Report.Log(ReportLevel.Info, "Mouse", "Mouse Left Click item 'Dropdown.Client' at 16;7.", repo.Dropdown.ClientInfo, new RecordItemIndex(4));
-            repo.Dropdown.Client.Click("16;7");
+            Report.Log(ReportLevel.Info, "Mouse", "Mouse Left Click item 'ApplicationUnderTest.AddTimeHours' at 26;-64.", repo.ApplicationUnderTest.AddTimeHoursInfo, new RecordItemIndex(3));
+            repo.ApplicationUnderTest.AddTimeHours.Click("26;-64");
             Delay.Milliseconds(0);
             
-            Report.Log(ReportLevel.Info, "Mouse", "Mouse Left Click item 'ApplicationUnderTest.AddAction' at 20;25.", repo.ApplicationUnderTest.AddActionInfo, new RecordItemIndex(5));
-            repo.ApplicationUnderTest.AddAction.Click("20;25");
+            Report.Log(ReportLevel.Info, "Mouse", "Mouse Left Click item 'ApplicationUnderTest.NgScope' at 534;226.", repo.ApplicationUnderTest.NgScopeInfo, new RecordItemIndex(4));
+            repo.ApplicationUnderTest.NgScope.Click("534;226");
             Delay.Milliseconds(0);
             
-            Report.Log(ReportLevel.Info, "Mouse", "Mouse Left Click item 'ApplicationUnderTest.Form' at 544;361.", repo.ApplicationUnderTest.FormInfo, new RecordItemIndex(6));
-            repo.ApplicationUnderTest.Form.Click("544;361");
+            Report.Log(ReportLevel.Info, "Mouse", "Mouse Left Click item 'ApplicationUnderTest.NgScope' at 534;229.", repo.ApplicationUnderTest.NgScopeInfo, new RecordItemIndex(5));
+            repo.ApplicationUnderTest.NgScope.Click("534;229");
             Delay.Milliseconds(0);
             
-            Report.Log(ReportLevel.Info, "Mouse", "Mouse Left Click item 'ApplicationUnderTest.Submit' at 45;16.", repo.ApplicationUnderTest.SubmitInfo, new RecordItemIndex(7));
-            repo.ApplicationUnderTest.Submit.Click("45;16");
+            Report.Log(ReportLevel.Info, "Mouse", "Mouse Left Click item 'ApplicationUnderTest.Submit' at 58;17.", repo.ApplicationUnderTest.SubmitInfo, new RecordItemIndex(6));
+            repo.ApplicationUnderTest.Submit.Click("58;17");
             Delay.Milliseconds(0);
             
         }
